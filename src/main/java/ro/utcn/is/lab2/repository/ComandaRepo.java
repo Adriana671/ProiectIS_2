6747ae5cca7f0ecb41d0b0bfdb471e2e174771c1
GeorgianaGeorgiu <georgiana.georgiu@ymail.com>
2019-11-28 09:04:34 +0200

    am adaugat clasele ProdusComandat,ProdusComandatRepo,UserComandaRepo

--- src/main/java/ro/utcn/is/lab2/repository/ComandaRepo.java
+++ src/main/java/ro/utcn/is/lab2/repository/ComandaRepo.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import ro.utcn.is.lab2.entity.Comanda;
+import ro.utcn.is.lab2.entity.Produse;
 
 public class ComandaRepo {
 	
